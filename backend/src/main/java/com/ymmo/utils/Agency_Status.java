0b8b9f26fe07d78c0b230996ca20d846030783eb
Shiokaa <tom.amaru09@gmail.com>
2026-03-12 16:47:17 +0100

    feat(enum): add Enums for database

---
+++ backend/src/main/java/com/ymmo/utils/Agency_Status.java
@@ -0,0 +1,5 @@
+package com.ymmo.utils;
+
+public enum Agency_Status {
+    OPEN, CLOSED, PLANNED, TEMPORILY_CLOSED
+}
---
+++ backend/src/main/java/com/ymmo/utils/Property_types.java
@@ -0,0 +1,5 @@
+package com.ymmo.utils;
+
+public enum Property_types {
+    HOUSE, APARTMENT, LAND, PARKING, BUILDING, OFFICE, RETAIL_SPACE, WAREHOUSE, BUSINESS_ASSETS
+}
