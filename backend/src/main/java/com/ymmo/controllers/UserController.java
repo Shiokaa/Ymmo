7295be13e9ca1b3567f4c05c6aeb2c4b0743a815
Shiokaa <tom.amaru09@gmail.com>
2026-03-15 00:36:57 +0100

    feat(user-registration): implement user registration logic with DTOs and validation

--- backend/src/main/java/com/ymmo/controllers/UserController.java
+++ backend/src/main/java/com/ymmo/controllers/UserController.java
@@ -1,28 +1,33 @@
 package com.ymmo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.ymmo.entities.User;
+import com.ymmo.dto.user.UserRequestDTO;
+import com.ymmo.dto.user.UserResponseDTO;
 import com.ymmo.services.UserService;
 
+import jakarta.validation.Valid;
 
-@Controller
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public String register(@RequestBody User user) {
-
-        userService.create(user);
-
-        String str = "Utilisateur créé !" + user;
-        return str;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@RequestBody @Valid UserRequestDTO userRequestDTO) {
 
+        UserResponseDTO userResponseDTO = userService.create(userRequestDTO);
+
+        return new ResponseEntity<>(userResponseDTO, HttpStatus.CREATED);
+    }
 }
--- backend/src/main/java/com/ymmo/services/UserService.java
+++ backend/src/main/java/com/ymmo/services/UserService.java
@@ -1,20 +1,55 @@
 package com.ymmo.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.ymmo.dto.user.UserRequestDTO;
+import com.ymmo.dto.user.UserResponseDTO;
 import com.ymmo.entities.User;
+import com.ymmo.hashing.Hashable;
 import com.ymmo.repositories.UserRepository;
 
-@Component
+@Service
 public class UserService {
 
-    @Autowired
-    private UserRepository repositories;
+    private UserRepository userRepository;
+    private Hashable hashing;
 
-    public void create(User user) {
-
-        repositories.save(user);
+    public UserService(UserRepository userRepository, Hashable hashing) {
+        this.userRepository = userRepository;
+        this.hashing = hashing;
     }
 
+    public UserResponseDTO create(UserRequestDTO userRequestDTO) {
+
+        if (userRepository.findByEmail(userRequestDTO.getEmail()) != null) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        User user = new User();
+
+        user.setFirstName(userRequestDTO.getFirstName());
+        user.setLastName(userRequestDTO.getLastName());
+        user.setEmail(userRequestDTO.getEmail());
+        user.setPasswordHash(hashing.bcryptHash(userRequestDTO.getPassword()));
+        user.setPhone(userRequestDTO.getPhone());
+
+        try {
+            user = userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+
+        userResponseDTO.setUuid(user.getUuid());
+        userResponseDTO.setFirstName(user.getFirstName());
+        userResponseDTO.setLastName(user.getLastName());
+        userResponseDTO.setEmail(user.getEmail());
+        userResponseDTO.setPhone(user.getPhone());
+
+        return userResponseDTO;
+    }
 }
