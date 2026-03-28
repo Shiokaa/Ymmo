690065dde77335e66582492db26b4e542fb47aec
Shioka <tom.amaru09@gmail.com>
2026-03-28 14:20:21 +0100

    feat(authentication): add email existence check in signup method to prevent duplicate registrations

--- backend/src/main/java/com/ymmo/services/AuthenticationService.java
+++ backend/src/main/java/com/ymmo/services/AuthenticationService.java
@@ -3,7 +3,10 @@ package com.ymmo.services;
 import com.ymmo.dtos.authentication.LoginUserDto;
 import com.ymmo.dtos.authentication.RegisterUserDto;
 import com.ymmo.entities.User;
+import com.ymmo.exception.EmailAlreadyExistsException;
 import com.ymmo.repositories.UserRepository;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,6 +37,12 @@ public class AuthenticationService {
                 .passwordHash(passwordEncoder.encode(input.getPassword()))
                 .phone(input.getPhone())
                 .build();
+
+        try {
+            userRepository.findByEmail(user.getEmail());
+        } catch (DataIntegrityViolationException e) {
+            throw new EmailAlreadyExistsException();
+        }
 
         return userRepository.save(user);
     }
