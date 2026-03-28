beb2f60d113c9c8bdc3255822150933690461094
Shioka <tom.amaru09@gmail.com>
2026-03-28 14:20:15 +0100

    feat(user): enhance LoginUserDto with email validation and password size constraint

--- backend/src/main/java/com/ymmo/dtos/authentication/LoginUserDto.java
+++ backend/src/main/java/com/ymmo/dtos/authentication/LoginUserDto.java
@@ -1,6 +1,8 @@
 package com.ymmo.dtos.authentication;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +10,9 @@ import lombok.Setter;
 @Setter
 public class LoginUserDto {
     @NotBlank
+    @Email
     private String email;
     @NotBlank
+    @Size(min = 6)
     private String password;
 }
