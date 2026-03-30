09e4974744407bcfcc12b224effbb833cb3cc4b7
Shiokaa <tom.amaru09@gmail.com>
2026-03-30 21:16:35 +0200

    feat(agency): implement update agency endpoint in AgencyController

--- backend/src/main/java/com/ymmo/controllers/AgencyController.java
+++ backend/src/main/java/com/ymmo/controllers/AgencyController.java
@@ -16,6 +16,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class AgencyController {
@@ -32,9 +34,18 @@ public class AgencyController {
     }
 
     @PostMapping("/agencies/create")
-    public ResponseEntity<GlobalResponse<AgencyResponseDto>> add(@RequestBody @Valid AgencyRequestDto agencyRequestDto) {
+    public ResponseEntity<GlobalResponse<AgencyResponseDto>> add(
+            @RequestBody @Valid AgencyRequestDto agencyRequestDto) {
         AgencyResponseDto agencyResponseDto = agencyService.create(agencyRequestDto);
 
         return new ResponseEntity<>(GlobalResponse.success(agencyResponseDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("agencies/{id}")
+    public ResponseEntity<GlobalResponse<AgencyResponseDto>> updateAgency(@PathVariable String id,
+            @RequestBody AgencyRequestDto agencyRequestDto) {
+        AgencyResponseDto agencyResponseDto = agencyService.updateAgency(agencyRequestDto, id);
+
+        return new ResponseEntity<>(GlobalResponse.success(agencyResponseDto), HttpStatus.OK);
     }
 }
