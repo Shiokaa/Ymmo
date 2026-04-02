a74465e1b8ca2f5b00cca602d8d1c90cbabea2f9
Shiokaa <tom.amaru09@gmail.com>
2026-04-02 14:59:18 +0200

    feat(user): add UserController to handle user-related requests

---
+++ backend/src/main/java/com/ymmo/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.ymmo.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ymmo.dtos.user.UserResponseDto;
+import com.ymmo.response.GlobalResponse;
+import com.ymmo.services.UserService;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<GlobalResponse<List<UserResponseDto>>> getAllUsers() {
+        return new ResponseEntity<>(GlobalResponse.success(userService.getAllUsers()), HttpStatus.OK);
+    }
+}
