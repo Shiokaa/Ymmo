bd474661b8e31347c9adefe3dcd57a487bda28c9
Shiokaa <tom.amaru09@gmail.com>
2026-04-02 17:03:01 +0200

    feat(user): add getUserById method to UserService and corresponding endpoint in UserController

--- backend/src/main/java/com/ymmo/controllers/UserController.java
+++ backend/src/main/java/com/ymmo/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ymmo.dtos.user.UserResponseDto;
@@ -22,5 +23,10 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<GlobalResponse<List<UserResponseDto>>> getAllUsers() {
         return new ResponseEntity<>(GlobalResponse.success(userService.getAllUsers()), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<GlobalResponse<UserResponseDto>> getUserById(@PathVariable String id) {
+        return new ResponseEntity<>(GlobalResponse.success(userService.getUserById(id)), HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/ymmo/services/UserService.java
+++ backend/src/main/java/com/ymmo/services/UserService.java
@@ -2,13 +2,16 @@ package com.ymmo.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.ymmo.dtos.user.UserResponseDto;
 import com.ymmo.entities.User;
+import com.ymmo.exceptions.ResourceNotFound;
 import com.ymmo.repositories.UserRepository;
+import com.ymmo.utils.ConvertType;
 
 @Service
 public class UserService {
@@ -37,5 +40,21 @@ public class UserService {
         }
 
         return userResponseDtos;
+    }
+
+    public UserResponseDto getUserById(String id) {
+        UUID uuid = ConvertType.stringToUuid(id);
+
+        User user = userRepository.findById(uuid).orElseThrow(ResourceNotFound::new);
+
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .role(user.getRole())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt()).build();
     }
 }
