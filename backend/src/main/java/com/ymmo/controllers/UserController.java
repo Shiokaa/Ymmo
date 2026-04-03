1a9cbfb80c3e06e08ffc5799153cb99fbfcdd304
Shiokaa <tom.amaru09@gmail.com>
2026-04-03 14:08:48 +0200

    feat(user): add updateUserProfileById method and UserUpdateProfilDto for user profile updates

--- backend/src/main/java/com/ymmo/controllers/UserController.java
+++ backend/src/main/java/com/ymmo/controllers/UserController.java
@@ -9,8 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ymmo.dtos.user.UserResponseDto;
+import com.ymmo.dtos.user.UserUpdateProfilDto;
 import com.ymmo.response.GlobalResponse;
 import com.ymmo.services.UserService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class UserController {
@@ -28,5 +34,12 @@ public class UserController {
     @GetMapping("/users/{id}")
     public ResponseEntity<GlobalResponse<UserResponseDto>> getUserById(@PathVariable String id) {
         return new ResponseEntity<>(GlobalResponse.success(userService.getUserById(id)), HttpStatus.OK);
+    }
+
+    @PutMapping("users/profile/{id}")
+    public ResponseEntity<GlobalResponse<UserResponseDto>> updateUserProfileById(@Valid @PathVariable String id,
+            @RequestBody UserUpdateProfilDto input) {
+        UserResponseDto userResponseDto = userService.updateUserProfileById(id, input);
+        return new ResponseEntity<>(GlobalResponse.success(userResponseDto), HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/ymmo/services/UserService.java
+++ backend/src/main/java/com/ymmo/services/UserService.java
@@ -2,13 +2,16 @@ package com.ymmo.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.ymmo.dtos.user.UserResponseDto;
+import com.ymmo.dtos.user.UserUpdateProfilDto;
 import com.ymmo.entities.User;
+import com.ymmo.exceptions.EmailAlreadyExistsException;
 import com.ymmo.exceptions.ResourceNotFound;
 import com.ymmo.repositories.UserRepository;
 import com.ymmo.utils.ConvertType;
@@ -46,6 +49,34 @@ public class UserService {
         UUID uuid = ConvertType.stringToUuid(id);
 
         User user = userRepository.findById(uuid).orElseThrow(ResourceNotFound::new);
+
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .role(user.getRole())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt()).build();
+    }
+
+    public UserResponseDto updateUserProfileById(String id, UserUpdateProfilDto input) {
+        UUID uuid = ConvertType.stringToUuid(id);
+
+        Optional<User> existingUser = userRepository.findByEmail(input.getEmail());
+        if (existingUser.isPresent() && !(existingUser.get().getId().equals(uuid))) {
+            throw new EmailAlreadyExistsException();
+        }
+
+        User user = userRepository.findById(uuid).orElseThrow(ResourceNotFound::new);
+
+        user.setFirstName(input.getFirstName());
+        user.setLastName(input.getLastName());
+        user.setEmail(input.getEmail());
+        user.setPhone(input.getPhone());
+
+        user = userRepository.save(user);
 
         return UserResponseDto.builder()
                 .id(user.getId())
