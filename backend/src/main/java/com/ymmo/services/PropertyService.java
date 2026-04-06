d3dac85edd652238d2eb7fee30e41e8f0cb39824
Shioka <tom.amaru09@gmail.com>
2026-04-06 18:27:26 +0200

    feat(property): enhance PropertyService to include property images in responses

--- backend/src/main/java/com/ymmo/services/PropertyService.java
+++ backend/src/main/java/com/ymmo/services/PropertyService.java
@@ -2,28 +2,47 @@ package com.ymmo.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.ymmo.dtos.agency.AgencyResponseDto;
+import com.ymmo.dtos.property.PropertyImageResponseDto;
 import com.ymmo.dtos.property.PropertyResponseDto;
 import com.ymmo.entities.Property;
+import com.ymmo.entities.PropertyImage;
+import com.ymmo.exceptions.ResourceNotFound;
+import com.ymmo.repositories.PropertyImageRepository;
 import com.ymmo.repositories.PropertyRepository;
+import com.ymmo.utils.ConvertType;
 
 @Service
 public class PropertyService {
     private PropertyRepository propertyRepository;
+    private PropertyImageRepository propertyImageRepository;
 
-    public PropertyService(PropertyRepository propertyRepository) {
+    public PropertyService(PropertyRepository propertyRepository, PropertyImageRepository propertyImageRepository) {
         this.propertyRepository = propertyRepository;
+        this.propertyImageRepository = propertyImageRepository;
     }
 
     public List<PropertyResponseDto> getAllProperties() {
         List<Property> properties = propertyRepository.findAll();
-
         List<PropertyResponseDto> propertyResponseDtos = new ArrayList<>();
 
         for (Property property : properties) {
+            List<PropertyImage> propertyImages = propertyImageRepository.findByPropertyId(property.getId());
+
+            List<PropertyImageResponseDto> propertyImageResponseDtos = new ArrayList<>();
+            for (PropertyImage propertyImage : propertyImages) {
+                PropertyImageResponseDto propertyImageResponseDto = PropertyImageResponseDto.builder()
+                        .imageUrl(propertyImage.getImageUrl())
+                        .description(propertyImage.getDescription())
+                        .isCover(propertyImage.getIsCover()).build();
+
+                propertyImageResponseDtos.add(propertyImageResponseDto);
+            }
+
             AgencyResponseDto agencyResponseDto = AgencyResponseDto.builder()
                     .id(property.getAgency().getId())
                     .name(property.getAgency().getName())
@@ -40,6 +59,7 @@ public class PropertyService {
             PropertyResponseDto propertyResponseDto = PropertyResponseDto.builder()
                     .id(property.getId())
                     .agency(agencyResponseDto)
+                    .propertyImages(propertyImageResponseDtos)
                     .title(property.getTitle())
                     .description(property.getDescription())
                     .type(property.getType())
@@ -57,5 +77,13 @@ public class PropertyService {
         }
 
         return propertyResponseDtos;
+    }
+
+    public PropertyResponseDto getPropertyById(String id) {
+        UUID uuid = ConvertType.stringToUuid(id);
+
+        Property property = propertyRepository.findById(uuid).orElseThrow(ResourceNotFound::new);
+
+        return PropertyResponseDto.builder().build();
     }
 }
