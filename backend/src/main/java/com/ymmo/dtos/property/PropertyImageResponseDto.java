822219dd8a99c00519a5af266f1fecdedef02d77
Shiokaa <tom.amaru09@gmail.com>
2026-04-08 16:49:53 +0200

    feat(dto): add JsonPropertyOrder annotation and UUID field to PropertyImageResponseDto

--- backend/src/main/java/com/ymmo/dtos/property/PropertyImageResponseDto.java
+++ backend/src/main/java/com/ymmo/dtos/property/PropertyImageResponseDto.java
@@ -1,5 +1,9 @@
 package com.ymmo.dtos.property;
 
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +11,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@JsonPropertyOrder({ "id", "imageUrl", "description", "isCover" })
 public class PropertyImageResponseDto {
+    private UUID id;
     private String imageUrl;
     private String description;
     private Boolean isCover;
