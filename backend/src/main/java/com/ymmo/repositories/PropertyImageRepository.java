b719c46d73b8aad1ccd9e968f35c4b93472de1b3
Shiokaa <tom.amaru09@gmail.com>
2026-04-08 16:49:29 +0200

    feat(repository): add PropertyImageRepository interface for PropertyImage entity management

---
+++ backend/src/main/java/com/ymmo/repositories/PropertyImageRepository.java
@@ -0,0 +1,12 @@
+package com.ymmo.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ymmo.entities.PropertyImage;
+
+@Repository
+public interface PropertyImageRepository extends JpaRepository<PropertyImage, UUID> {
+}
