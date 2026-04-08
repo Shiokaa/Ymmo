f0aaf3624aad9344e95f346e8f56dc2de9d70b7e
Shiokaa <tom.amaru09@gmail.com>
2026-04-08 16:49:06 +0200

    feat(exception): add IsCoverAlreadyExistsException for handling cover existence errors

---
+++ backend/src/main/java/com/ymmo/exceptions/IsCoverAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.ymmo.exceptions;
+
+public class IsCoverAlreadyExistsException extends RuntimeException {
+    public IsCoverAlreadyExistsException() {
+        super("IS_COVER_ALREADY_EXIST");
+    }
+}
