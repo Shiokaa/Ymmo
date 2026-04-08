aeefe24f9e84a538098b2bc017152d7d5adba8b4
Shiokaa <tom.amaru09@gmail.com>
2026-04-08 16:49:48 +0200

    feat(property): remove property field from PropertyImageRequestDto

--- backend/src/main/java/com/ymmo/dtos/property/PropertyImageRequestDto.java
+++ backend/src/main/java/com/ymmo/dtos/property/PropertyImageRequestDto.java
@@ -1,14 +1,11 @@
 package com.ymmo.dtos.property;
 
-import com.ymmo.entities.Property;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PropertyImageRequestDto {
-    private Property property;
     private String imageUrl;
     private String description;
     private Boolean isCover;
