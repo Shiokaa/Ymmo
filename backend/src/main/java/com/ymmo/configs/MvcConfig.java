53e8dfbd76583968a2ffd3dc8e707b940c5aa6ac
Shiokaa <tom.amaru09@gmail.com>
2026-04-27 17:23:41 +0200

    feat(property): add MvcConfig for handling resource uploads

---
+++ backend/src/main/java/com/ymmo/configs/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.ymmo.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    private static final String UPLOAD_DIR = System.getProperty("user.dir") + "/uploads/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(
+                        "file:" + UPLOAD_DIR);
+    }
+}
